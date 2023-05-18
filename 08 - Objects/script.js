f3e3ca24aa5994752204745bd7b702f2341ffc6e
jason walker <jwalkerus3@yahoo.com>
2023-05-18 07:02:01 -0500

    08 - Objects

---
+++ 08 - Objects/script.js
@@ -0,0 +1,28 @@
+let player = {
+    age: 46, 
+    name: 'Jason',
+    isActive: true,
+    outfit: {
+        color: 'forest green',
+        size: '5xl',
+        cost: '150',
+        isNew: true
+    }
+};
+console.log(player);
+console.log(player.name);
+console.log(player['name']);
+
+player.isActive = false;
+console.log(player);
+
+player.health = 100;
+console.log(player);
+
+delete player.health; // Becareful using delete, could cause errors
+console.log(player);
+
+console.log(player.outfit.cost);
+
+player.outfit.size = "4xl";
+console.log(player.outfit.size);
